9cf4499ca18ee12fb5cc2346395193ce4133ad9d
Computerfr33k <computerfreak@computerfr33k.com>
2015-05-18 18:14:59 -0500

    DB Work

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@
 $(function () {
   'use strict';
   // Initialize the IDB
-  var db = new Dexie("tasks");
+  var db = new Dexie("OnTrack");
   db.version(1).stores({
+    // subject (string), title (string), dueDate (string),
+    // completed (bool), notify (bool)
+    task: '++id,subject,title,dueDate,completed,notify',
+    settings: 'label,value'
   });
 
   main();
@@ -12,8 +16,29 @@ $(function () {
   function main() {
     // Initialize Material Bootstrap Theme
     $.material.init();
-    initUI();
+
+    var date = new Date(Date.parse(task.dueDate));
 
     db.open();
+    addTask({
+      subject: "CSC 360",
+      title: "Finish Prototype",
+      dueDate: date.toLocaleString(),
+      completed: false,
+      notify: true
+    });
+  }
+
+  /*
+   * @param task (containing information to be added to the DB) | @type object
+   */
+  function addTask(task) {
+    db.task.add({
+      subject: task.subject,
+      title: task.title,
+      dueDate: task.dueDate,
+      completed: task.completed,
+      notify: task.notify
+    });
   }
 });
