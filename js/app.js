09a8f8c64c2e508200aec6262d58e9f7fa5baf6c
Computerfr33k <computerfreak@computerfr33k.com>
2015-05-18 17:19:04 -0500

    Dexie wrapper for IDB.

--- js/app.js
+++ js/app.js
@@ -2,13 +2,11 @@
 
 $(function () {
   'use strict';
-  var db;
-  var DBOpenRequest;
+  // Initialize the IDB
+  var db = new Dexie("tasks");
+  db.version(1).stores({
+  });
 
-  // create a blank instance of the object that is used to transfer data into the IDB.
-  var newItem = [
-    {taskSubject: "", taskTitle: "", taskDueDate: "", taskCompleted: "no"}
-  ];
   main();
 
   function main() {
@@ -16,53 +14,6 @@ $(function () {
     $.material.init();
     initUI();
 
-    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-    window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-    window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-
-    DBOpenRequest = window.indexedDB.open("toDoList", 1);
-  }
-
-  DBOpenRequest.onsuccess = function(event) {
-    db = DBOpenRequest.result;
-  }
-  
-  function initUI() {
-    if(localStorage.getItem("options") == "undefined" || localStorage.getItem("options") == null) {
-      initStorage();
-    } else {
-      var options = getOptions();
-      if(options.showNotifications) {
-        $("#notifications").prop("checked", "checked");
-      }
-    }
-    
-    $("#notifications").click(function() {
-      var options = getOptions();
-      options.showNotifications = $("#notifications").prop("checked");
-      
-      localStorage.setItem("options", JSON.stringify(options));
-    });
-  }
-  
-  function getOptions() {
-    return JSON.parse(localStorage.getItem("options"));
-  }
-  
-  function saveOptions(options) {
-    localStorage.setItem("options", JSON.stringify(options));
-  }
-
-  function initStorage() {
-    var options = {
-      "showNotifications": true
-    };
-    var tasks = {
-      
-    };
-    
-    // LocalStorage can only store key/value as strings
-    localStorage.setItem("options", JSON.stringify(options));
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    db.open();
   }
 });
