0f66e78c4579badc21d5df9be526af417586ee7b
Computerfr33k <computerfreak@computerfr33k.com>
2015-05-20 17:21:40 -0500

    Deleting Tasks now works

--- js/app.js
+++ js/app.js
@@ -2,26 +2,37 @@
 
 $(function () {
   'use strict';
-  var taskList;
   // Initialize the IDB
   var db = new Dexie("OnTrack");
-  db.version(1).stores({
-    // subject (string), title (string), dueDate (string),
-    // completed (bool), notify (bool)
-    task: '++id,subject,title,dueDate,completed,notify',
-    settings: 'label,value'
-  });
-
+  var editBtns = $('.edit-item-btn'),
+    removeBtns = $('.remove-item-btn');
   main();
 
   function main() {
-    db.open();
+    db.version(2).stores({
+      // subject (string), title (string), dueDate (string),
+      // completed (bool), notify (bool)
+      task: 'id++,subject,title,dueDate,completed,notify',
+      settings: 'label,value'
+    });
+    // open database for storing tasks
+    db.open().then(function () {
+      initTasks();
+    });
 
-    initTasks();
-    
     $('#dueDatePicker').datetimepicker();
+    $('#warning-alert').hide();
+    $('#editTask-Btn').hide();
 
+    // setup click handler for add task button
     $('#addTask-Btn').click(function () {
+      // prevent Fields from being blank
+      if ($.trim($('#subject').val()) === "" || $.trim($('#title').val()) === "" || $.trim($('#dueDate').val()) === "") {
+        $('#warning-alert').fadeIn();
+        return;
+      }
+      $('#warning-alert').fadeOut();
+
       var task = {
         subject: $('#subject').val(),
         title: $('#title').val(),
@@ -29,28 +40,31 @@ $(function () {
         completed: $('#completed').prop('checked'),
         notify: $('#notify').prop('checked')
       };
-
-      console.log("Adding Task: " + JSON.stringify(task));
       addTask(task);
-      $('#subject').val('');
-      $('#title').val('');
-      $('#dueDate').val('');
-      $('#completed').prop('checked', '');
-      $('#notify').prop('checked', 'checked');
+    });
+    
+    // click handler for saving edited task
+    $('#editTask-Btn').click(function() {
+      // switch the buttons since we are now adding a task.
+      $('#addTask-Btn').show();
+      $('#editTask-Btn').hide();
     });
   }
 
   function initTasks() {
-    db.task.orderBy("id").each(function (tasks) {      
+    db.task.orderBy("id").each(function (tasks) {
       addTaskToTable(tasks);
+    }).then(function () {
+      $.material.init();
     });
+    clearFields();
   }
-  
+
   function addTaskToTable(task) {
     // Add all tasks to table
-      var complete = (task.completed) ? 'checked' : '';
-      var notify = (task.notify) ? 'checked' : '';
-      
+    var complete = (task.completed) ? 'checked' : '';
+    var notify = (task.notify) ? 'checked' : '';
+
     $('.list:last').append('<tr><td class="id hide ot-valign">' + task.id + '</td>\
               <td class="subject ot-valign">' + task.subject + '</td>\
               <td class="title ot-valign">' + task.title + '</td>\
@@ -71,9 +85,10 @@ $(function () {
                   </label>\
                 </div>\
               </td>\
-              <td><button class="btn btn-primary">Edit</button><button class="btn btn-primary">Delete</button></td></tr>\
+              <td><button class="btn btn-primary edit-item-btn">Edit</button><button class="btn btn-primary remove-item-btn">Delete</button></td></tr>\
       ');
-      $.material.init();
+    refreshCallbacks();
+    $.material.init();
   }
 
   /*
@@ -93,14 +108,46 @@ $(function () {
     }).then(function (id) {
       task.id = id;
       addTaskToTable(task);
+      clearFields();
     });
   }
 
   function editTask(task) {
-
   }
 
-  function deleteTask(task) {
+  function deleteTask(id) {
+    db.task.delete(Number(id)).then(function (r) {
+      console.log(JSON.stringify(r));
+    });
+  }
 
+  function clearFields() {
+    $('#subject').val('');
+    $('#title').val('');
+    $('#dueDate').val('');
+    $('#completed').prop('checked', '');
+    $('#notify').prop('checked', 'checked');
+  }
+
+  function refreshCallbacks() {
+    removeBtns = $(removeBtns.selector);
+    editBtns = $(editBtns.selector);
+
+    removeBtns.click(function () {
+      var itemId = $(this).closest('tr').find('.id').text();
+      deleteTask(itemId);
+
+      $(this).closest('tr').remove();
+    });
+    
+    // Send Existing Data to Add Task inputs (re-using the same UI for both adding and editing).
+    editBtns.click(function() {
+      // switch the buttons since we are now editing a task.
+      $('#addTask-Btn').hide();
+      $('#editTask-Btn').show();
+      
+      var itemId = Number($(this).closest('tr').find('.id').text());
+      console.log(itemId);
+    });
   }
 });
